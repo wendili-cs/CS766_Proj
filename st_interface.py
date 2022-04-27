9a3912c41cec43c3e1dc4908eca5165708aee686
Wendi <wendili.academic@qq.com>
2022-04-27 12:34:30 +0800

    Add image upload

--- st_interface.py
+++ st_interface.py
@@ -1,6 +1,25 @@
 import warnings
 import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image
 
 app_title = "A Robust License Plate Recognition System based on Domain Adaptation"
 st.set_page_config(page_title=app_title)
 
+uploaded_file = st.file_uploader("Upload a licence plate picture to do the recognition", type=['png', 'jpg'])
+
+@st.cache(show_spinner=False)
+def load_local_image(uploaded_file):
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()  
+        image = np.array(Image.open(BytesIO(bytes_data)))
+        return image
+    else:
+        return None
+
+uploaded_file = st.sidebar.file_uploader(" ")
+image = load_local_image(uploaded_file)
+
+if image is not None:
+    st.image(image)
