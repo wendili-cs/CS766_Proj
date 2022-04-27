88959850a2f4a1b3a0df639a53fc986ef1ae5e8e
Wendi <wendili.academic@qq.com>
2022-04-27 12:39:43 +0800

    Add streamlit interface

--- st_interface.py
+++ st_interface.py
@@ -2,11 +2,10 @@ import warnings
 import streamlit as st
 import numpy as np
 from PIL import Image
+from io import BytesIO
 
 app_title = "A Robust License Plate Recognition System based on Domain Adaptation"
-st.set_page_config(page_title=app_title)
 
-uploaded_file = st.file_uploader("Upload a licence plate picture to do the recognition", type=['png', 'jpg'])
 
 @st.cache(show_spinner=False)
 def load_local_image(uploaded_file):
@@ -17,8 +16,15 @@ def load_local_image(uploaded_file):
     else:
         return None
 
-uploaded_file = st.sidebar.file_uploader(" ")
-image = load_local_image(uploaded_file)
+def main():
+    st.set_page_config(page_title=app_title)
 
-if image is not None:
-    st.image(image)
+    uploaded_file = st.file_uploader("Upload a licence plate picture to do the recognition", type=['png', 'jpg'])
+    uploaded_file = st.sidebar.file_uploader(" ")
+    image = load_local_image(uploaded_file)
+
+    if image is not None:
+        st.image(image)
+
+if __name__ == "__main__":
+    main()
